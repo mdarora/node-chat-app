9b3ee4731248fec0411f670cdfcd9d57a8046d19
MD Arora <mdarora0001@gmail.com>
2021-06-12 18:55:46 +0530

    showing new user search result in modal by fetch request

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -78,4 +78,74 @@ const getChats = async () =>{
     }
 }
 
+document.getElementById('searchUserForm').addEventListener('submit', async (e) =>{
+    e.preventDefault();
+    const queryName = e.target[0].value;
+    const newSearchRes = document.getElementById('new-search-res');
+    const userListSpinner = document.getElementById('userListSpinner');
+    const newUserList = document.getElementById('new-users-list');
+
+
+    try {
+        newUserList.innerHTML = '';
+        userListSpinner.hidden = false;
+        newSearchRes.hidden = true;
+        const res = await fetch('/searchUsers', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify({queryName})
+        });
+        const result = await res.json();
+    
+        if(result.error){
+            userListSpinner.hidden = true;
+            newSearchRes.hidden = false;
+            newSearchRes.textContent = result.error;
+            newSearchRes.classList.add('text-danger');
+        } else {
+            const allusers = result.map((element, index)=>{
+                const {name, _id, email} = element;
+               return {name, _id, email}
+            });
+            userListSpinner.hidden = true;
+            newSearchRes.hidden = true;
+
+            allusers.forEach(element => {
+                newUserList.innerHTML += `
+                    <li class="chats-list-item" >
+                         <a href="/addChat/${element._id}" >
+                           <div class="Chat-avtaar" >
+                                 <figure>
+                                     <i class="fas fa-user-circle"></i>
+                                 </figure>
+                           </div>
+                           <div class="chat-content  text-truncate" >
+                                <h5 class='chat-name text-truncate' >
+                                        ${element.name}
+                                </h5>
+                                <p class="chat-last-msg text-truncate" >
+                                        ${element.email}
+                                </p>
+                           </div>
+                         </a>
+                     </li>
+                `
+            });
+        }
+        
+    } catch (error) {
+        console.log(error);
+        newSearchRes.hidden = false;
+        newSearchRes.textContent = 'Something went wrong!';
+        userListSpinner.hidden = true;
+    }
+});
+    
+    
+
+
+
 window.addEventListener('load', getChats);
