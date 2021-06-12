ef931ea6f408632dc3c6f76b46fdf0deaa56a412
MD Arora <mdarora0001@gmail.com>
2021-06-12 15:40:09 +0530

    initial setup done

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+dotenv.config({path:".env"});
+const app = express();
+
+
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+global.socketio = io;
+
+app.use(express.static(__dirname + '/public/'));
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use(require("./routes/auth"));
+app.use(require("./routes/router"));
+
+
+require('./db/dbConn');
+server.listen(process.env.PORT, ()=>{
+    console.log(`Listening on port : ${process.env.PORT}`); 
+});
+
