2b2cacb09e87cf348cbe57881cee758540444583
mdarora <mdarora0001@gmail.com>
2022-04-16 04:52:19 -0400

    Removed otp verification system , had too much bugs and was not implemented properly

--- routes/auth.js
+++ routes/auth.js
@@ -22,39 +22,8 @@ const transporter = nodemailer.createTransport({
 
 //////////////////////////-Global variables-///////////////////////////////////
 
-let regUser = {}
-let generatedOTP;
 let loginPageResponse = '';
-let resetMethod = 'post';
 let resetResponse = '';
-let generatedresetOTP = 0;
-let resetId;
-
-//////////////////////////-Functions-///////////////////////////////////
-
-const sendOtpViaMail = (email, subject, text) =>{
-    const sendOTP = new Promise((resolve, reject) =>{
-        const otp = Math.floor(Math.random() * 1000000);
-        const mailOptions = {
-            from : process.env.MAIL,
-            to : email,
-            subject : subject,
-            text : `${text}\n${otp}`
-        }
-
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.log(error);
-                reject({error : 'OTP sending failed'});
-            } else {
-                console.log(info.response);
-                resolve({otp});
-            }
-        });
-    });
-
-    return sendOTP
-}
 
 ///////////////////////////-Routes-/////////////////////////////////////
 router.get('/login', authLogin, (req, res)=>{
@@ -97,8 +66,6 @@ router.get('/register', authLogin,  (req, res) =>{
 
 router.post("/register", authLogin,  async (req, res)=>{
 
-    regUser = {};
-    generatedOTP = 0;
     const {name, email, password, cpassword} = req.body;
 
     if (!name || !email || !password || !cpassword) {
@@ -116,17 +83,22 @@ router.post("/register", authLogin,  async (req, res)=>{
         if (findByEmail){
             return res.render('register', {error : "Email already registered"});
         }
-        
 
-        regUser = {name, email, password}
-        generatedOTP = await sendOtpViaMail(email, 'OTP Verification code for Chat-app', 'Your One Time Password (OTP) for Chat-app is');
-
-        if (generatedOTP.error){
-            return res.render('register', {error : "Something went wrong!"});
-        } else {
-            generatedOTP = generatedOTP.otp;
-            res.redirect('/otpverification');
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const regUser = {
+            name,
+            email,
+            password : hashedPassword
         }
+        const newUser = new User(regUser);
+
+        const result = await newUser.save();
+        if (!result._id){
+            console.log(result);
+            res.render('register', {response: 'Something went wrong!'});
+        }
+        loginPageResponse = 'Registerated successfully';
+        return res.redirect('/login');
 
     } catch (error) {
         console.log(error);
@@ -134,132 +106,54 @@ router.post("/register", authLogin,  async (req, res)=>{
     }
 });
 
-
-router.get('/otpverification', authLogin, (req, res) =>{
-    if(!generatedOTP || generatedOTP === 0){
-        return res.redirect('/login');
-    }
-    res.render('otpVerification', {response: `OTP has been sent to your E-mail. (Please check SPAM too.)`});
-});
-
-router.post('/otpverification', authLogin, async (req, res) => {
-    if (parseInt(req.body.otp) !== generatedOTP) {
-        return res.render('otpVerification', {response: 'Invalid OTP'});
-    }
-
-    try {
-        const hashedPassword = await bcrypt.hash(regUser.password, 12);
-        const newUser = new User({
-            name : regUser.name,
-            email : regUser.email,
-            password : hashedPassword
-        });
-
-        const result = await newUser.save();
-        if (!result._id){
-            console.log(result);
-            res.render('otpVerification', {response: 'Something went wrong!'});
-        }
-        regUser = {};
-        generatedOTP = 0;
-        loginPageResponse = 'Registerated successfully';
-        // Todo : send mail of Registeration
-        return res.redirect('/login');
-
-    } catch (error) {
-        console.log(error);
-        res.render('otpVerification', {response: 'Something went wrong!'});
-    }
-});
-
-
 router.get('/reset-password', authLogin, (req, res) =>{
-    resetMethod = 'post';
     resetResponse = '';
-    res.render('resetPassword', {resetMethod, resetResponse});
+    res.render('resetPassword', {resetResponse});
 });
 
 
 router.post('/reset-password', authLogin, async (req, res) => {
     const email = req.body.email;
-    if (!email) {
-        resetResponse = 'Please enter your E-mail.';
-        return res.render('resetPassword', {resetMethod, resetResponse});
+    const newPassword = req.body.newPassword;
+    const newCPassword = req.body.newCPassword;
+
+    if (!email || !newPassword || !newCPassword){
+        resetResponse = 'All fields are required';
+        return res.render('resetPassword', {resetResponse});
+    } else if (newPassword !== newCPassword){
+        resetResponse = 'Both passwords must be same';
+        return res.render('resetPassword', {resetResponse});
     }
 
     try {
         const findByEmail = await User.findOne({email: email}, {_id : 1});
         if (!findByEmail){
             resetResponse = 'Invalid E-mail.';
-            return res.render('resetPassword', {resetMethod, resetResponse});
+            return res.render('resetPassword', {resetResponse});
         }
-        generatedresetOTP = await sendOtpViaMail(email,'Reset Password request for Chat-app account', 'Your One Time Password (OTP) to reset your account\'s password is');
 
-        if (generatedresetOTP.error){
-            resetResponse = 'Something went wrong!';
-            return res.render('resetPassword', {resetMethod, resetResponse});
-        } else {
-            generatedresetOTP = generatedresetOTP.otp;
-            resetId = findByEmail._id;
-        }
-        resetResponse = 'OTP sent to your E-mail';
-        resetMethod = 'put';
-        return res.render('resetPassword', {resetMethod, resetResponse});
-
-    } catch (error) {
-        console.log(error);
-        resetResponse = 'Something went wrong!';
-        return res.render('resetPassword', {resetMethod, resetResponse});
-    }
-});
-
-router.post('/verify-reset-otp', authLogin, async (req, res) => {
-    if(generatedresetOTP == 0 || !resetId){
-        return res.redirect('/login');
-    }
-
-    const enteredResetOtp = req.body.enteredResetOtp;
-    const newPassword = req.body.newPassword;
-    const newCPassword = req.body.newCPassword;
-
-    if (!enteredResetOtp || !newPassword || !newCPassword){
-        resetResponse = 'All fields are required';
-        return res.render('resetPassword', {resetMethod, resetResponse});
-    } else if (newPassword !== newCPassword){
-        resetResponse = 'Both passwords must be same';
-        return res.render('resetPassword', {resetMethod, resetResponse});
-    } else if (parseInt(enteredResetOtp) !== generatedresetOTP){
-        resetResponse = 'Invalid OTP';
-        return res.render('resetPassword', {resetMethod, resetResponse});
-    }
-
-    try {
         const hashedPassword = await bcrypt.hash(newPassword, 12);
-        const updatePass = await User.updateOne({_id:resetId}, {
+        const updatePass = await User.updateOne({_id:findByEmail._id}, {
             $set: {
                 password: hashedPassword
             }
         });
         if (updatePass.ok){
-            generatedresetOTP = 0;
-            resetId = '';
-            resetMethod = 'post';
             resetResponse = '';
             loginPageResponse = 'Password reset successful';
-            // Todo : send mail of password reseting
             return res.redirect('/login');
         } else {
             console.log(updatePass);
             resetResponse = 'Something went wrong!';
-            return res.render('resetPassword', {resetMethod, resetResponse});
+            return res.render('resetPassword', {resetResponse});
         }
+
     } catch (error) {
         console.log(error);
         resetResponse = 'Something went wrong!';
-        return res.render('resetPassword', {resetMethod, resetResponse});
+        return res.render('resetPassword', {resetResponse});
     }
 });
-
 
 router.get("/logout", (req, res) => {
     res.clearCookie("jwtoken");
